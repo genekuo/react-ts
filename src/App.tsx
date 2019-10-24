6683dca92724e8446b344cbc3dabd60065db96fe
KuoYu-chiung <genekuo@cybersoft4u.com>
2019-10-24 10:30:38 +0800

    create app template

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './App.scss'
+
+const App:React.FC = () => (
+    <div className='container'>
+        <h1>Hello World, React! Type</h1>
+    </div>
+)
+
+export default App
